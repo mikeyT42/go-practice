2da1654306a747a53fc5c12abbd89accb2ac7baa
Michael Torres <mrmojorisen0@gmail.com>
2025-03-10 23:33:15 -0400

    Getting some more in. I also ran gofmt.

--- arithmetic/main.go
+++ arithmetic/main.go
@@ -1,16 +1,21 @@
 package main
 
 import (
+	"fmt"
 	"os"
 	"os/exec"
 )
 
 func main() {
-    cmd := exec.Command("clear")
-    cmd.Stdout = os.Stdout
-    cmd.Stderr = os.Stderr
-    err := cmd.Run()
-    if err != nil {
-        panic("Couldn't execute the clear command.")
-    }
+	cmd := exec.Command("clear")
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	err := cmd.Run()
+	if err != nil {
+		panic("Couldn't execute the clear command.")
+	}
+	fmt.Print(
+		"---------------------------------------------------------------",
+		"\t\tWelcome",
+		"---------------------------------------------------------------")
 }
