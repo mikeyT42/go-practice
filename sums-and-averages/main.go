46750e71510be6d0ce5f3e97c841a5718fbca12c
Michael Torres <mrmojorisen0@gmail.com>
2025-03-20 23:15:45 -0400

    A bunch of stuff got done.

--- sums-and-averages/main.go
+++ sums-and-averages/main.go
@@ -5,8 +5,11 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"strconv"
 	"strings"
 )
+
+const maxItems = 10
 
 type LoopControl int
 
@@ -31,6 +34,24 @@ func (e *TooManyError) Error() string {
 
 func (e *InputError) Error() string {
 	return fmt.Sprintf("You did not input valid input.\nerror:\n%v", e.Err)
+}
+
+type Sums struct {
+	positive float32
+	negative float32
+	overall  float32
+}
+
+type Counts struct {
+	positive uint16
+	negative uint16
+	overall  uint16
+}
+
+type Averages struct {
+	positive float32
+	negative float32
+	overall  float32
 }
 
 // -----------------------------------------------------------------------------
@@ -61,7 +82,6 @@ func main() {
 // -----------------------------------------------------------------------------
 func inputLoop() LoopControl {
 	const sentinel = '\n'
-	const maxItems = 10
 
 	fmt.Printf(
 		"Please input up to %d floating point or integer numbers. Seperate\n"+
@@ -70,25 +90,75 @@ func inputLoop() LoopControl {
 	reader := bufio.NewReader(os.Stdin)
 	input, err := reader.ReadString(sentinel)
 
-	validate(input, err)
+	ns, err := validate(input, err)
+	switch err := err.(type) {
+	case *InputError:
+		fmt.Printf(
+			"You did not input valid input [%s]\nerror:\n%v\n\n",
+			input[:len(input)-1], err.Err)
+		return Continue
+	case *TooManyError:
+		fmt.Printf(
+			"You input too many numbers, please only input up to %d.\n\n",
+			maxItems,
+		)
+		return Continue
+	case *NoInputError:
+		return Stop
+	}
+
+	var s Sums
+	var c Counts
+	//var a Averages
+	sumAndCount(ns, &s, &c)
 
 	return Continue
 }
 
 // -----------------------------------------------------------------------------
-func validate(input string, err error) ([]float32, error) {
+func validate(input string, err error) ([maxItems]float32, error) {
 	if err != nil {
-		return nil, &InputError{err}
+		return [maxItems]float32{}, &InputError{err}
 	}
-	input = input[:len(input)-1]
+	input = strings.TrimSpace(input)
 	if len(input) == 0 {
-		return nil, &NoInputError{}
+		return [maxItems]float32{}, &NoInputError{}
 	}
 
-	for _, sn := range strings.Split(input, " ") {
+	iSplit := strings.Split(input, " ")
+	if len(iSplit) == 0 {
+		return [maxItems]float32{}, &NoInputError{}
 	}
-
-	var f []float32
+	var f [maxItems]float32
+	for i, sn := range iSplit {
+		if i == maxItems {
+			return [maxItems]float32{}, &TooManyError{}
+		}
+		if sn == "" {
+			continue
+		}
+		floc, err := strconv.ParseFloat(sn, 32)
+		if err != nil {
+			return [maxItems]float32{}, &InputError{}
+		}
+		f[i] = float32(floc)
+	}
 
 	return f, nil
+}
+
+// -----------------------------------------------------------------------------
+func sumAndCount(ns [maxItems]float32, s *Sums, c *Counts) {
+	for i := range len(ns) {
+		fmt.Printf("%f\n", ns[i])
+		if ns[i] >= 0 {
+			s.positive += ns[i]
+			c.positive += 1
+		} else {
+			s.negative += ns[i]
+			c.negative += 1
+		}
+		s.overall += ns[i]
+		c.overall += 1
+	}
 }
--- sums-and-averages/main_test.go
+++ sums-and-averages/main_test.go
@@ -7,11 +7,12 @@ import (
 
 // -----------------------------------------------------------------------------
 func TestValidateInputErrorArg(t *testing.T) {
+	const veX int = 0
 	err := errors.New("I am a testing error")
 
 	ve, e := validate("some input", err)
-	if ve != nil {
-		t.Errorf("actual %v :: expected %v", ve, nil)
+	if len(ve) == veX {
+		t.Errorf("actual %v :: expected %v", ve, veX)
 	}
 	_, ok := e.(*InputError)
 	if !ok {
@@ -21,12 +22,81 @@ func TestValidateInputErrorArg(t *testing.T) {
 
 // -----------------------------------------------------------------------------
 func TestValidationNoInputError(t *testing.T) {
+	const veX int = 0
 	ve, e := validate("\n", nil)
-	if ve != nil {
-		t.Errorf("actual %v :: expected %v", ve, nil)
+	if len(ve) == veX {
+		t.Errorf("actual %v :: expected %v", ve, veX)
 	}
 	_, ok := e.(*NoInputError)
 	if !ok {
 		t.Errorf("actual %T :: expected *NoInputError", e)
+	}
+}
+
+// -----------------------------------------------------------------------------
+func TestValidationNoInputErrorWhitespace(t *testing.T) {
+	const veX int = 0
+	ve, e := validate("          \n", nil)
+	if len(ve) == veX {
+		t.Errorf("actual %v :: expected %v", ve, veX)
+	}
+	_, ok := e.(*NoInputError)
+	if !ok {
+		t.Errorf("actual %T :: expected *NoInputError", e)
+	}
+}
+
+// -----------------------------------------------------------------------------
+func TestValidationTooManyError(t *testing.T) {
+	const veX int = 0
+	ve, e := validate("1 2 3 4 5 6 7 8 9 10 11", nil)
+	if len(ve) == veX {
+		t.Errorf("actual %v :: expected %v", ve, veX)
+	}
+	_, ok := e.(*TooManyError)
+	if !ok {
+		t.Errorf("actual %T :: expected *TooManyError", e)
+	}
+}
+
+// -----------------------------------------------------------------------------
+func TestValidationOkWhitespaceOverload(t *testing.T) {
+	const veX int = 3
+	ve, e := validate("  1  2 3    \n", nil)
+	if e != nil {
+		t.Errorf("actual %T :: expected %T", e, nil)
+	}
+	if len(ve) == veX {
+		t.Errorf("actual %v :: expected %v", ve, veX)
+	}
+}
+
+// -----------------------------------------------------------------------------
+func TestValidationOk(t *testing.T) {
+	const veX int = 3
+	ve, e := validate("1 2 3\n", nil)
+	if e != nil {
+		t.Errorf("actual %T :: expected %T", e, nil)
+	}
+	if len(ve) == veX {
+		t.Errorf("actual %v :: expected %v", ve, veX)
+	}
+}
+
+// -----------------------------------------------------------------------------
+func TestSumsAndCount(t *testing.T) {
+	sEx := Sums{10, -5, 5}
+	cEx := Counts{1, 1, 2}
+	f := [maxItems]float32{10, -5}
+	var s Sums
+	var c Counts
+
+	sumAndCount(f, &s, &c)
+
+	if s != sEx {
+		t.Errorf("actual %v :: expected %v", s, sEx)
+	}
+	if c != cEx {
+		t.Errorf("actual %v :: expected %v", c, cEx)
 	}
 }
