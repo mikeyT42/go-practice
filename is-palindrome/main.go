9b1b93a5bb59c3447db4f6b46b234c0b62e1878f
Michael Torres <mrmojorisen0@gmail.com>
2025-03-23 22:28:10 -0400

    Ok, I got my first test in.

--- is-palindrome/main.go
+++ is-palindrome/main.go
@@ -1,1 +1,68 @@
 package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+)
+
+type LoopControl int
+
+const (
+	Continue LoopControl = iota
+	Stop
+)
+
+// -----------------------------------------------------------------------------
+func main() {
+	cmd := exec.Command("clear")
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	err := cmd.Run()
+	if err != nil {
+		panic("Couldn't execute the clear command.")
+	}
+	fmt.Print(
+		"------------------------------------------------------------------\n",
+		"                           Welcome\n",
+		"------------------------------------------------------------------\n")
+
+	var lc LoopControl = Continue
+	for lc == Continue {
+		lc = inputLoop()
+	}
+
+	fmt.Print(
+		"------------------------------------------------------------------\n",
+		"                           Thank you\n",
+		"------------------------------------------------------------------\n")
+}
+
+// -----------------------------------------------------------------------------
+func inputLoop() LoopControl {
+	const sentinel = '\n'
+
+	fmt.Print(
+		"Please enter a string that is a palindrome; if you want to exit\n",
+		"then just hit enter. It can be a sentence or a word.\n\n",
+	)
+	reader := bufio.NewReader(os.Stdin)
+	input, err := reader.ReadString(sentinel)
+	if err != nil {
+		fmt.Println("There was an error reading input. Please try again.")
+		return Continue
+	}
+
+	input = cleanInput(input)
+
+	return Continue
+}
+
+// -----------------------------------------------------------------------------
+func cleanInput(s string) string {
+	cl := strings.TrimSpace(s)
+
+	return cl
+}
--- is-palindrome/main_test.go
+++ is-palindrome/main_test.go
@@ -1,1 +1,16 @@
 package main
+
+import "testing"
+
+// -----------------------------------------------------------------------------
+func TestCleanInputNoSpace(t *testing.T) {
+	const (
+		in   = "nursesrun\n"
+		inEx = "nursesrun"
+	)
+
+	inAc := cleanInput(in)
+	if inAc != inEx {
+		t.Errorf("actual %s :: expected %s", inAc, inEx)
+	}
+}
